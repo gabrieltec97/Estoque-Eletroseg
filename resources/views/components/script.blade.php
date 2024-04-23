e79dc82d1e76d1cad6b929ea913508dde3c35797
Gabriel Pereira <gabrieldeveloper23@hotmail.com>
2024-04-23 18:24:05 -0300

    first commit

---
+++ resources/views/components/script.blade.php
@@ -0,0 +1,20 @@
+ <!-- Bootstrap core JavaScript-->
+ <script src="{{ url('sb/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ url('sb/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{ url('sb/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{ url('sb/js/sb-admin-2.min.js') }}"></script>
+
+    <!-- Page level plugins -->
+    <script src="{{ url('sb/vendor/chart.js/Chart.min.js') }}"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="{{ url('sb/js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{ url('sb/js/demo/chart-pie-demo.js')}}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <script src="https://kit.fontawesome.com/757bf4501b.js" crossorigin="anonymous"></script>

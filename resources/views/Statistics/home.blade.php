0fde3bd867075c87ff35a47808ef6e39dc1bd8b0
Gabriel Pereira <gabrieldeveloper23@hotmail.com>
2024-04-25 12:25:56 -0300

    teste de layout

--- resources/views/Statistics/home.blade.php
+++ resources/views/Statistics/home.blade.php
@@ -2,5 +2,25 @@
 
 @section('content')
 
-
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-6">
+                        <select name="" id="" class="form-control">
+                            <option value="" selected>Condomínios</option>
+                            <option value="">Opção 1</option>
+                            <option value="">Opção 2</option>
+                        </select>
+                    </div>
+                    <div class="col-4">
+                        teste
+                    </div>
+                    <div class="col-4">
+                        teste
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
